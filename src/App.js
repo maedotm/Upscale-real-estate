81d32312d2a7a929c4978db7c1080c336cac4b75
Madi <maedotmetsihet0@gmail.com>
2025-05-21 16:01:13 +0300

    landing page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'; // Ensure this is correct
 
-function App() {
+const App = () => {
+  const texts = [
+    "Welcome to Our Website!",
+    "Explore Our Features!",
+    "Join Us Today!",
+    "Contact Us for More Info!"
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="landing-page">
+      <div className="background"></div> {/* Background div */}
+      {texts.map((text, index) => (
+        <div key={index} className="sliding-text">
+          {text}
+        </div>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
